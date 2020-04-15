2fa4682774c3632fdfadfb35ea53a74e8fa67eab
Administrator <Administrator@MS-HOGSNYDZDISC>
2020-04-15 12:29:32 +0800

    添加项目文件。

---
+++ AppCoreSample/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AppCoreSample.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
